0dfbe0eda01c31730794600e2a00fd290897ba56
ht325ver0 <hts325313@gmail.com>
2026-02-01 23:19:10 +0900

    buttonコンポーネントをadd

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
+import Header from './components/Header.vue';
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
+  <div class="min-h-screen bg-background">
+    <Header />
   <div>
     <a href="https://vite.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
@@ -11,7 +14,9 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
+
   <HelloWorld msg="Vite + Vue" />
+  </div>
 </template>
 
 <style scoped>
---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+    <header class="justify-end flex p-4 w-full sticky top-0">
+            <Button variant="outline">
+                Button
+            </Button>
+            <Button variant="outline">
+                Button
+            </Button>
+            <Button variant="outline">
+                Button
+            </Button>
+    </header>
+
+</template>
