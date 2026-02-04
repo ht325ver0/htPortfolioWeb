1ceb8b02a606d92bd2cb10821e97a2cbf4a3e272
ht325ver0 <hts325313@gmail.com>
2026-02-04 15:23:03 +0900

    デフォルトのスタイルシートのボタンのデザイン部分削除

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,10 +4,11 @@ import { Button } from '@/components/ui/button'
 
 
 <template>
-    <header class="justify-end flex gap-4 p-4 w-full sticky top-0">
+    <header class="justify-end flex gap-4 p-4 w-full sticky top-0 bg-blue-500">
         <Button variant="ghost" size="lg">
             Home
         </Button>
+        
         <Button variant="ghost" size="lg">
             History
         </Button>
@@ -15,4 +16,5 @@ import { Button } from '@/components/ui/button'
             About Me
         </Button>
     </header>
+ 
 </template>
