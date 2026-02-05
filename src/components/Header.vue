c6befd80eb5a5339425b1a8eec3ddd0537e47d05
ht325ver0 <sou10aka6@docomo.ne.jp>
2026-02-05 15:22:43 +0900

    セパレータ追加

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,19 +1,25 @@
 <script setup lang="ts">
 import { Button } from '@/components/ui/button'
+import { Separator } from '@/components/ui/separator'
 </script>
 
 
 <template>
-    <header class="justify-end flex gap-4 p-4 w-full sticky top-0 bg-white/50">
+    <header class="justify-end flex items-center gap-4 p-4 w-full sticky top-0 bg-white/50 backdrop-blur-md h-20">
         <Button variant="ghost" size="lg">
             Home
         </Button>
+        
+        <Separator orientation="vertical" />
+        
         <Button variant="ghost" size="lg">
             History
         </Button>
+        
+        <Separator orientation="vertical" />
+        
         <Button variant="ghost" size="lg">
             About Me
         </Button>
     </header>
- 
 </template>
