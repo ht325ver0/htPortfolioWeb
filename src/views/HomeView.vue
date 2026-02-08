e429c8961107af506b5ed1a4b3d8d71f7b8d8a7f
ht325ver0 <sou10aka6@docomo.ne.jp>
2026-02-08 12:39:14 +0900

    imageのサイズ調整

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
+import ProductCard from '../components/ProductCard.vue'
+
 </script>
 
 <template>
+
     <h1>Home View</h1>
+    <div class="grid grid-cols-3 gap-x-15 gap-y-20 p-20">
+        <ProductCard v-for="n in 8" :key="n" />
+    </div>
 </template>
