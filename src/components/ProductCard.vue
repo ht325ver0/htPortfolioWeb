ba7461a55d734861c1f3eb69c829c92d5867fbd7
ht325ver0 <sou10aka6@docomo.ne.jp>
2026-02-13 12:20:02 +0900

    ホバーサイズ変更

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -3,7 +3,6 @@ import { ref } from 'vue'
 import { AspectRatio } from '@/components/ui/aspect-ratio'
 import {
   Card,
-  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
@@ -13,6 +12,12 @@ import {
     TagsInputItem, 
     TagsInputItemText } 
 from '@/components/ui/tags-input'
+
+const props = defineProps({
+    title: String,
+    description: String,
+    tags: Array,
+})
 
 const frameworksRef = ref(['Vue', 'TypeScript'])
 </script>
@@ -24,17 +29,34 @@ const frameworksRef = ref(['Vue', 'TypeScript'])
       alt="Photo by Drew Beamer"
       class="h-full w-full rounded-lg object-cover dark:brightness-[0.2] dark:grayscale"
     >
-    <Card class="absolute bottom-1 left-1 right-1 bg-white/50 backdrop-blur-md opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+    <Card class="absolute 
+        bottom-0
+        left-0
+        right-0
+        top-50
+        bg-white/50 
+        backdrop-blur-md 
+        opacity-0 
+        transition-opacity 
+        duration-300 
+        group-hover:opacity-100"
+    >
         <CardHeader>
             <CardTitle class="text-3xl">Card Title</CardTitle>
             <TagsInput 
                 v-model="frameworksRef" 
-                class="w-full bg-transparent rounded-lg border-none shadow-none px-0 gap-2""
+                class="w-full 
+                bg-transparent 
+                border-none 
+                shadow-none 
+                px-0 
+                gap-2"
             >
                 <TagsInputItem 
                     v-for="item in frameworksRef" 
                     :key="item" 
                     :value="item"
+                    class="bg-white/100"
                 >
                     <TagsInputItemText />
                 </TagsInputItem>
